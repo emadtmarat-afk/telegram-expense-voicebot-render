4c947cbbce5cca4a7108e54dc94ad096dfbdd48d
emadtmarat-afk <emad.tmarat@gmail.com>
2025-10-04 01:55:15 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -17,7 +17,7 @@ SHEET_ID = os.environ.get("GOOGLE_SHEET_ID")
 scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
 creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
 client = gspread.authorize(creds)
-sheet = client.open_by_key(GOOGLE_SHEET_ID).sheet1
+sheet = client.open_by_key(SHEET_ID).sheet1
 
 def download_voice(file_id):
     # Step 1: Get file path
